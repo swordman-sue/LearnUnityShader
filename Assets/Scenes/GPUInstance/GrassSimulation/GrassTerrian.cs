da340cd1bcfff59dfe8f226c9e096c6c4af537bd
swordman sue <jy02789801@gmail.com>
2024-10-19 12:24:27 +0800

    切换至master branch

--- Assets/Scenes/GPUInstance/GrassSimulation/GrassTerrian.cs
+++ Assets/Scenes/GPUInstance/GrassSimulation/GrassTerrian.cs
@@ -82,12 +82,9 @@ public class GrassTerrian : MonoBehaviour
             var vertex2 = vertices[triIndex[i + 1]];
             var vertex3 = vertices[triIndex[i + 2]];
             // 计算三角形面积
-            // 编译错误，暂时屏蔽
-            //var arena = MathTools.CalculateTriangleArea(vertex1, vertex2, vertex3);
+            var arena = MathTools.CalculateTriangleArea(vertex1, vertex2, vertex3);
             // 法向量
-            // 编译错误，暂时屏蔽
-            //Vector3 normal = MathTools.CalculateTriangleNormal(vertex1, vertex2, vertex3).normalized;
-            Vector3 normal = new Vector3(1, 1, 1);
+            Vector3 normal = MathTools.CalculateTriangleNormal(vertex1, vertex2, vertex3).normalized;
 
             // 一个三角形面生成 grassCount 个
             for (var j = 0; j < grassCount; j++)
@@ -97,9 +94,7 @@ public class GrassTerrian : MonoBehaviour
                 Vector2 texOffset = Vector2.zero;
                 Vector4 texParams = new Vector4(texScale.x, texScale.y, texOffset.x, texOffset.y);
                 // 三角形内随机采样
-                // 编译错误，暂时屏蔽
-                //Vector3 randPos = MathTools.RandomTriangle(vertex1, vertex2, vertex3);
-                Vector3 randPos = new Vector3(1, 1, 1);
+                Vector3 randPos = MathTools.RandomTriangle(vertex1, vertex2, vertex3);
                 // 向法线方向偏移
                 randPos += normal.normalized * 0.5f * grassQuadSize.y;
 
